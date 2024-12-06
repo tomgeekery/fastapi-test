d5c267d27472615cac88c74e5d1b958fcfd9b70b
Chris Keller <chris@teamgeekery.dev>
2024-12-06 07:34:30 -0500

    Adjust origins

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost:8080"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
